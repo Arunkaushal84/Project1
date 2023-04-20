402928bcd95b8103feb79f8f1c2a54309a07f456
Jung In Kim <vindictuslbp@gmail.com>
2023-04-20 18:43:57 -0400

    Fixed an weird visual error regarding with toggling buttons in the main menu.

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,8 @@ const containerB = document.querySelector('#containerB');
 const buttonB = document.querySelector('#toggleButtonB');
 
 buttonA.addEventListener('click', () => {
+    document.getElementById("output-define").innerHTML = "";
+    document.getElementById("input-define").value = "";
     containerA.classList.toggle('d-none');
     const isHiddenA = containerA.classList.contains('d-none');
     console.log(`Container A is hidden: ${isHiddenA}`);
@@ -14,6 +16,8 @@ buttonA.addEventListener('click', () => {
 });
 
 buttonB.addEventListener('click', () => {
+    document.getElementById("output-define").innerHTML = "";
+    document.getElementById("input-define").value = "";
     containerB.classList.toggle('d-none');
     const isHiddenB = containerB.classList.contains('d-none');
     console.log(`Container B is hidden: ${isHiddenB}`);
