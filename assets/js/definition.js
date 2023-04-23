5f0bb79630eeaac6d6e2f6ef332faccd44ef2324
phillipkujawa <phillip.kujawa@me.com>
2023-04-23 17:55:33 -0400

    adding comments to translation JavaScript code

--- assets/js/definition.js
+++ assets/js/definition.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
     // Function to fetch API data then display the definitions of a user inputted word from searchDefinition().
     function defineWord(word){
 
-        var apiKey = localStorage.getItem('dictionaryApiKey'); //"ac4a6086-68c9-4b49-a9cc-ad60fc350977";
+        var apiKey = localStorage.getItem('dictionaryApiKey');
         queryURL = "https://www.dictionaryapi.com/api/v3/references/collegiate/json/" + word + "?key=" + apiKey;
 
         fetch(queryURL)
